16bab36421bf1915fd117cf84ea2de2ffe25f314
gorkavillara <gorkavillara@gmail.com>
2023-10-10 12:08:07 +0200

    Tema 07 - LocalStorage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-import chalk from "chalk"
+const formulario = document.querySelector("form") as HTMLFormElement
+const input = document.querySelector("input[name=nombre]") as HTMLInputElement
+const saludo = document.querySelector("div#saludo") as HTMLDivElement
+const spanNombre = document.querySelector("span#nombre") as HTMLSpanElement
+const botonLogout = document.querySelector("button#logout") as HTMLButtonElement
 
-console.log(chalk.blue("Hola mundo"))
+const checkDisplay = () => {
+    const nombreUsuario = localStorage.getItem("nombre")
+    if (!nombreUsuario) {
+        saludo.style.display = "none"
+        formulario.style.display = "block"
+        return
+    }
+
+    saludo.style.display = "block"
+    formulario.style.display = "none"
+
+    spanNombre.innerText = nombreUsuario
+}
+
+const logout = () => {
+    localStorage.removeItem("nombre")
+    checkDisplay()
+}
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const nombre = input.value
+
+    localStorage.setItem("nombre", nombre)
+
+    console.log("El nombre de usuario se ha guardado correctamente")
+    checkDisplay()
+})
+
+botonLogout.addEventListener("click", logout)
+
+checkDisplay()
