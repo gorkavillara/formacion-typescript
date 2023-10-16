9b0f2157006c451dcf9d58d3102d01843e7b2da4
gorkavillara <gorkavillara@gmail.com>
2023-10-16 12:00:46 +0200

    Ejercicios eventos

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,26 @@
-let db: IDBDatabase
-const openDB = window.indexedDB.open("db_tareas", 1)
+const imagen = document.querySelector("img")!
 
-openDB.addEventListener("error", () =>
-    console.error("Error abriendo la IndexedDB")
-)
-
-openDB.addEventListener("success", () => {
-    console.log("IndexedDB abierta correctamente")
-    db = openDB.result
+imagen.addEventListener("load", () => {
+    console.log("Anchura de la imagen:", imagen.width)
+    console.log("Altura de la imagen:", imagen.height)
 })
 
-openDB.addEventListener("upgradeneeded", () => {
-    db = openDB.result
-
-    db.onerror = () => {
-        console.error("Error creando la IndexedDB.")
-    }
-
-    db.createObjectStore("tareas", { keyPath: "id", autoIncrement: true })
+window.addEventListener("pointerdown", (event) => {
+    if (event.button !== 2) return
+    event.preventDefault()
+    alert("No se puede hacer clic con el botón secundario")
 })
 
-const tareas = document.querySelector("ol") as HTMLOListElement
-const form = document.querySelector("form") as HTMLFormElement
-const todoTitle = document.querySelector("#task-name") as HTMLInputElement
+const divAzul = document.querySelector("div#div-azul") as HTMLDivElement
+// Creamos el evento personalizado.
+const eventoPasoRaton = new CustomEvent("eventoPasoRaton")
 
-const muestraTareas = () => {
-    const transaction = db.transaction(["tareas"], "readwrite")
-    const objectStore = transaction.objectStore("tareas")
-    const query = objectStore.getAll()
-    query.onsuccess = () => console.log(query.result)
-    // TODO: Se muestren las tareas también en el div con id = tasks
-}
+// Escuchamos el evento.
+divAzul.addEventListener("eventoPasoRaton", () => {
+    console.log("¡Se ha disparado el evento personalizado!")
+})
 
-const nuevaTarea = (e: SubmitEvent) => {
-    e.preventDefault()
-    const newTodo = { tarea: todoTitle.value }
-
-    const transaction = db.transaction(["tareas"], "readwrite")
-    const objectStore = transaction.objectStore("tareas")
-
-    const query = objectStore.add(newTodo)
-    query.onsuccess = () => (todoTitle.value = "")
-    transaction.oncomplete = () => muestraTareas()
-    transaction.onerror = () => console.log("Error en la transacción")
-}
-
-form.addEventListener("submit", nuevaTarea)
+// Disparamos el evento cuando se pase el ratón por encima del div.
+divAzul.addEventListener("mouseover", () => {
+    divAzul.dispatchEvent(eventoPasoRaton)
+})
